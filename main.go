c3c0d5d2867d3b722f7074955e05f0792fdf779a
Gonçalo Mestre <goncalo.mestre@freiheit.com>
2024-03-22 17:54:34 +0000

    Add structs for second step

--- main.go
+++ main.go
@@ -35,6 +35,30 @@ type Planet struct {
 	URL            string    `json:"url"`
 }
 
+type Resident struct {
+	Name      string        `json:"name"`
+	Height    string        `json:"height"`
+	Mass      string        `json:"mass"`
+	HairColor string        `json:"hair_color"`
+	SkinColor string        `json:"skin_color"`
+	EyeColor  string        `json:"eye_color"`
+	BirthYear string        `json:"birth_year"`
+	Gender    string        `json:"gender"`
+	Homeworld string        `json:"homeworld"`
+	Films     []string      `json:"films"`
+	Species   []interface{} `json:"species"`
+	Vehicles  []interface{} `json:"vehicles"`
+	Starships []string      `json:"starships"`
+	Created   time.Time     `json:"created"`
+	Edited    time.Time     `json:"edited"`
+	URL       string        `json:"url"`
+}
+
+type PlanetwithResidents struct {
+	PlanetName     string   `json:"name"`
+	ResidentsNames []string `json:"residents"`
+}
+
 func getPlanetsList() ([]Planet, error) {
 	var planets []Planet
 	planetsRequestURI := "https://swapi.dev/api/planets/"
